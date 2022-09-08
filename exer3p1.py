279ae01d283d2912a90cd9bbe1c7a7f6a5070b71
Greg K <gkreisman_cep@qq.com>
2022-09-08 10:23:03 +0800

    first commit

---
+++ exer3p1.py
@@ -0,0 +1,6 @@
+def right_justify(instring):
+    print((70-len(instring))*"."+instring) 
+    #mult is distributive even in text so move the string to the second position and add string
+
+
+right_justify("montypython")  
